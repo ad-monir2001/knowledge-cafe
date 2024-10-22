7c461688db500fc4712ab86bd057fc95f9efcfa9
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-10-22 07:21:50 +0600

    environment setup for knowledge cafe project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl text-green-500 text-center my-4">
+        Hello tailwind css
+      </h1>
+    </>
+  );
+}
+
+export default App;
